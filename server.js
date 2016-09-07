b4deda2254e5f3e02a16914cd768888f32bf99f9
JP DeVries <mail@devries.jp>
2016-09-07 02:08:01 +0200

    re ordering radio options

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ Twig = require("twig"),
 express = require('express'),
 app = express();
 
-app.use(express.static(__dirname));
-
 app.get('/', function(req, res) {
   res.render('index.twig', {
 
   });
 });
+
+app.use(express.static(__dirname));
 
 app.listen(process.env.PORT || 1189);
 
